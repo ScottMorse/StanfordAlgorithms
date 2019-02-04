752d311f3a3a774f02bac68c03febf75a90512f9
Scott Morse <scottmorsedev@gmail.com>
2019-02-04 14:34:24 -0600

    Initial

---
+++ karatsuba_multiplication.py
@@ -0,0 +1,31 @@
+x = 5678
+y = 1234
+
+def karatsuba(x,y):
+    stx = str(x)
+    sty = str(y)
+
+    nx = len(stx)
+    ny = len(sty)
+
+    if nx == 1 or ny == 1:
+        return x * y
+
+    base = max(nx,ny)
+    half_base = base // 2
+
+    bx = nx - half_base
+    by = ny - half_base
+
+    a = x // 10**(half_base)
+    b = x % 10**(half_base)
+    c = y // 10**(half_base)
+    d = y % 10**(half_base)
+
+    ac = karatsuba(a,c)
+    bd = karatsuba(b,d)
+    abcd = karatsuba((a+b),(c+d))
+
+    sub = abcd - ac - bd
+
+    return (ac * 10**(2*half_base)) + ((abcd - ac - bd) * 10**(half_base)) + (bd)
